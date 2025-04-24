9ed15751f1746d9b5cf898d80ba4bae4663ccc9e
21elisa06 <eli.de.farfalla@gmail.com>
2025-04-24 16:59:28 +0200

    Antipatico

--- login.php
+++ login.php
@@ -1,3 +1,24 @@
+<?php
+    require_once "utils/pdo.php";
+    if($_SERVER[REQUEST_METHOD]=="POST"){
+        $sql = " SELECT * FROM Aziende WHERE email = :email ; ";
+        try{
+            $query = $pdo -> prepare($sql);
+            $query -> bindParam("email", $_POST[email], PDO::PARAM_STR);
+            $res = $query -> execute();
+            $data = $query -> fetch();
+            if (password_verify($_POST[password], $data[0] -> password)) {
+                $_SESSION[loggato][email] = $data[0] -> email;
+                header("location: ./");
+            }
+        }catch(\PDOException $ex){
+            die("Errore: $ex");
+        }
+        echo 'login';
+        exit(-1);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +27,7 @@
     <title> Accedi </title>
 </head>
 <body class="d-flex align-items-center justify-content-center min-vh-100">
+    
     <form class="w-25 rounded-3 bg-altea-blue p-4 shadow-sm">
         <div class="mb-3">
             <label for="email" class="form-label">Indirizzo e-mail</label>
